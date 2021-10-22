d6c3e9d3709e04abf85cf481077faa99b4aae468
Kostas Aznavouridis <kaznavouridis@gmail.com>
2021-10-22 20:46:32 +0300

    (L)

--- gui/ktima.py
+++ gui/ktima.py
@@ -49,20 +49,20 @@ class KtimaUI(QWidget):
 
         self.console = Console(size=(600, None), parent=self)
 
-        self.tabs = QTabWidget(self)
+        self.tabs = QTabWidget(parent=self)
         self.tabs.setDocumentMode(True)
 
-        self.settingsTab = SettingsTab(size=(600, None), parent=self)
+        self.settingsTab = SettingsTab(size=(700, None), parent=self)
         self.tabs.addTab(self.settingsTab, "Ρυθμίσεις")
-        self.filesTab = FilesTab(size=(600, None), parent=self)
+        self.filesTab = FilesTab(size=(700, None), parent=self)
         self.tabs.addTab(self.filesTab, "Ενημέρωση")
-        self.countTab = CountTab(size=(600, None), parent=self)
+        self.countTab = CountTab(size=(700, None), parent=self)
         self.tabs.addTab(self.countTab, "Καταμέτρηση")
-        self.organizeTab = OrganizeTab(size=(600, None), parent=self)
+        self.organizeTab = OrganizeTab(size=(700, None), parent=self)
         self.tabs.addTab(self.organizeTab, "Οργάνωση")
-        self.paradosiTab = ParadosiTab(size=(600, None), parent=self)
+        self.paradosiTab = ParadosiTab(size=(700, None), parent=self)
         self.tabs.addTab(self.paradosiTab, "Παράδοση")
-        self.anartisiTab = AnartisiTab(size=(600, None), parent=self)
+        self.anartisiTab = AnartisiTab(size=(700, None), parent=self)
         self.tabs.addTab(self.anartisiTab, "Ανάρτηση")
 
         self.appLayout.addWidget(self.tabs)
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 
 from PyQt5.QtGui import QFont, QIcon
 from PyQt5.QtWidgets import QApplication
-
+from at.gui.utils import get_dpi
 from atktima.gui.ktima import KtimaUI
 from atktima.settings import *
 
@@ -13,7 +13,10 @@ if __name__ == "__main__":
     myappid = 'com.aztool.atktima.app'
     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
-    SEGOE = QFont(FONT, FONTSIZE)
+    if get_dpi() < 120:
+        SEGOE = QFont(FONT, FONTSIZE)
+    else:
+        SEGOE = QFont(FONT, FONTSIZE - 2)
 
     app = QApplication(sys.argv)
     app.setWindowIcon(QIcon(APPICON))
