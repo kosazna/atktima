14e21c0f9d4c71d15f074e1ce1eb2fc82c653738
Kostas Aznavouridis <kaznavouridis@gmail.com>
2021-10-24 11:57:33 +0300

    (L)

--- app/interface/paradosi.py
+++ app/interface/paradosi.py
@@ -9,7 +9,7 @@ import pandas as pd
 from at.auth.client import Authorize, AuthStatus, licensed
 from at.auth.utils import load_lic
 from at.gui.components import *
-from at.gui.utils import set_size
+from at.gui.utils import HORIZONTAL, VERTICAL, set_size
 from at.gui.worker import run_thread
 from at.io.copyfuncs import batch_copy_file, copy_file
 from at.logger import log
@@ -42,6 +42,8 @@ class ParadosiTab(QWidget):
         layout = QVBoxLayout()
         layout.setContentsMargins(2, 4, 2, 0)
         labelLayout = QHBoxLayout()
+        checksLayout = QVBoxLayout()
+        metadataLayout = QHBoxLayout()
 
         self.fullname = Label(icon='person-fill',
                               label=state['fullname'],
@@ -56,12 +58,63 @@ class ParadosiTab(QWidget):
                             label=state['meleti'],
                             parent=self)
 
+        path_mapping = {'LocalData': paths.get_localdata(),
+                        'ParadosiData': paths.get_paradosidata()}
+        self.shapeFolder = PathSelector(label="Φάκελος Χωρικών",
+                                        selectortype='folder_in',
+                                        mapping=path_mapping,
+                                        orientation=VERTICAL,
+                                        combosize=(180, 24),
+                                        labelsize=(None, 24),
+                                        parent=self)
+
+        self.mdbFolder = FolderInput(label="Φάκελος Βάσεων",
+                                     orientation=VERTICAL,
+                                     labelsize=(None, 24),
+                                     parent=self)
+        self.checkMdbOrganized = CheckInput(label="Οργανωμένες",
+                                            checked=True,
+                                            parent=self)
+
+        self.checkMetadata = CheckInput(label="Metadata",
+                                        checked=False,
+                                        parent=self)
+        self.dateMetadata = StrInput(label="Ημερομηνία",
+                                     labelsize=(100, 24),
+                                     editsize=(150, 24),
+                                     parent=self)
+
+        self.checkEmptyShapes = CheckInput(label="Δημιουργία κενών shapefile",
+                                           checked=False,
+                                           parent=self)
+
+        self.folderOutput = PathSelector(label="Φάκελος που θα δημιουργηθεί η παράδοση",
+                                         selectortype='folder_in',
+                                         mapping=path_mapping,
+                                         orientation=VERTICAL,
+                                         combosize=(180, 24),
+                                         labelsize=(None, 24),
+                                         parent=self)
+
+        self.dateMetadata.setPlaceholder("dd/mm/yyyy")
+
         labelLayout.addWidget(self.fullname)
         labelLayout.addWidget(self.username)
         labelLayout.addWidget(self.company, stretch=2, alignment=Qt.AlignLeft)
         labelLayout.addWidget(self.meleti)
         layout.addLayout(labelLayout)
         layout.addWidget(HLine())
+        layout.addWidget(self.shapeFolder)
+        layout.addWidget(self.mdbFolder)
+        layout.addWidget(HLine())
+        checksLayout.addWidget(self.checkMdbOrganized)
+        checksLayout.addWidget(self.checkEmptyShapes)
+        metadataLayout.addWidget(self.checkMetadata)
+        metadataLayout.addWidget(self.dateMetadata, stretch=2, alignment=Qt.AlignLeft)
+        layout.addLayout(checksLayout)
+        layout.addLayout(metadataLayout)
+        layout.addWidget(self.folderOutput)
+        layout.addWidget(HLine(), stretch=2, alignment=Qt.AlignTop)
 
         self.setLayout(layout)
 
@@ -74,7 +127,7 @@ if __name__ == '__main__':
     app.setFont(SEGOE)
     app.setStyle('Fusion')
 
-    ui = ParadosiTab(size=(600, None))
+    ui = ParadosiTab(size=(None, 600))
     ui.setStyleSheet(cssGuide)
     ui.show()
 
